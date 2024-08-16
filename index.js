5a6956935add53b7ca227adbbc63fef511d0cc1e
Ankit Panwar <ankitpanwar212585@gmail.com>
2024-08-16 18:03:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+// Serve static files
+app.use(express.static(path.resolve("./public")));
+
+// Route for serving the index.html
+app.get("/", (req, res) => {
+  return res.sendFile(path.resolve("./public/index.html"));
+});
+
+// Start the server on port 9001
+server.listen(9001, () =>
+  console.log("Server is running on port 9001")
+);
